8a8dd576eb4ee8f49f1cec0a53202edf6fbc3dc8
MorganNdungu <morgansingh7@gmail.com>
2023-03-29 12:58:37 +0300

    added code

---
+++ dom.js
@@ -0,0 +1,8 @@
+function SumNumber(){
+    let num1=document.getElementById("num1").value;
+    let num2=document.getElementById("num2").Value;
+
+    let sum=parseInt(num1)+ parseInt(num2);
+    alert(sum);
+
+}
