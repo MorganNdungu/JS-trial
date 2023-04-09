5ecb03c4fcc55ae6ed429da70f431835ea6293a0
MorganNdungu <ndungumorgan20@gmail.com>
2023-04-10 01:01:43 +0300

    updated analog code

--- analog.js
+++ analog.js
@@ -7,12 +7,13 @@ setInterval(clockRotating, 1000);
 function clockRotating(){
     var date=new Date();
     var getSeconds=date.getSeconds()/60;
-    var getMinutes=Date().getMinutes()/60;
+    var getMinutes=date.getMinutes()/60;
     var getHours=date.getHours()/12;
 
     secondHand.style.transform="rotate("+getSeconds*360 +"deg)";
+    minHand.style.transform="rotate("+getMinutes*360 +"deg)";
     hourHand.style.transform="rotate("+getHours*360 +"deg)";
-    minHand.style.transform="rotate("+getSeconds*360 +"deg)";
+
 
 
 
