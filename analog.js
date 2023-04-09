98da641e9d2e4a1da5ad763b1ea7a3d4213768d6
MorganNdungu <ndungumorgan20@gmail.com>
2023-04-09 21:20:12 +0300

    added  calc code

--- analog.js
+++ analog.js
@@ -1,27 +1,27 @@
-let secondHand=document.querySelector("#sec")
-let minHand=document.querySelector("#min")
-let hourHand=document.querySelector("#hr")
+let secondHand=document.querySelector("#sec");
+let minHand=document.querySelector("#min");
+let hourHand=document.querySelector("#hr");
 
-setInterval(clockRotating, 1000)
+setInterval(clockRotating, 1000);
 
 function clockRotating(){
     var date=new Date();
     var getSeconds=date.getSeconds()/60;
-    var getMinutes=date.getMinutes()/60;
+    var getMinutes=Date().getMinutes()/60;
     var getHours=date.getHours()/12;
 
-    secondHand.style.transform="rotate("+getSeconds*360 +"deg)"
-    hourHand.style.transform="rotate("+getHours*360 +"deg)"
-    minHand.style.transform="rotate("+getMinutes*360 +"deg)"
+    secondHand.style.transform="rotate("+getSeconds*360 +"deg)";
+    hourHand.style.transform="rotate("+getHours*360 +"deg)";
+    minHand.style.transform="rotate("+getSeconds*360 +"deg)";
 
 
 
     
    
 
-    // console.log(getSeconds)
-    // console.log(getMinutes)
-    // console.log(getHours)
+     console.log(getSeconds);
+     console.log(getMinutes);
+     console.log(getHours);
 
 
 
